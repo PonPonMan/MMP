b09ded36fc46f8ac7dd273c943dff4117c45f4d2
傅文城 <fwc@meitu.com>
2017-06-26 13:11:05 +0800

    fwc:thymeleaf标签 th:

--- src/main/java/com/controller/HtmlController.java
+++ src/main/java/com/controller/HtmlController.java
@@ -1,12 +1,20 @@
 package com.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.module.device.bean.Device;
+import com.module.device.dao.DeviceDao;
+
 @Controller
 public class HtmlController {
+	@Autowired
+	private DeviceDao deviceDAO;
 
 	@RequestMapping("/hello/{name}")
 	public String hello(@PathVariable("name") String name, Model model) {
@@ -16,6 +24,9 @@ public class HtmlController {
 
 	@RequestMapping("/index")
 	public String hello(Model model) {
-		return "index";
+		List<Device> devices = deviceDAO.findAll();
+		model.addAttribute("devices", devices);
+		model.addAttribute("message", "fej");
+		return "list";
 	}
 }
