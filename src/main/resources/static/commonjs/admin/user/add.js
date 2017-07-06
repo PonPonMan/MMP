69d467b870a1907022484750eda78f3006fba1f7
fwc <382133955@qq.com>
2017-07-07 00:21:11 +0800

    jq validate

--- src/main/resources/static/commonjs/admin/user/add.js
+++ src/main/resources/static/commonjs/admin/user/add.js
@@ -3,12 +3,23 @@ $(function() {
 		var index = parent.layer.getFrameIndex(window.name);
 		parent.layer.close(index);
 	});
+//	$('#form').validate({
+//		submitHandler : function(form) {
+//			var $form = $(form), data = $form.serialize(); //序列化表单数据
+//			//这里是jquery表单验证通过的时候执行的操作，比如这里，表单验证通过的时候执行了jquery的ajax的post操作      
+////			$.post('js/test.json', {
+////				data : data
+////			}, function(d) {
+////				if (d.Flag) {
+////					alert("如果返回为真，将执行这里代码")
+////				} else {
+////
+////				}
+////			}, 'json');
+//		}
+//	});
+	$('#form').validate();
 	$('#saveBtn').click(function() {
 		$('#form').submit();
-	});
-	$('input[name="repassword"]').blur(function() {
-		if($(this).val() != $('input[name="password"]').val()){
-			layer.msg('两次密码不一致');
-		}
 	});
 });
